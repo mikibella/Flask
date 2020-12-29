1d483ebc44201f6da55797c4490b24f630c4dc8a
Michael <bellamichael@web.de>
2020-12-29 19:21:07 +0100

    Flask Tutorial Part 1-3

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,30 @@
 from app import app
-
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Michael'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+from flask import render_template, flash, redirect
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
